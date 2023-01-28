3a7813b343298f617685cfeaec4584d6458e531b
Nouster <djebali.mohamed88@gmail.com>
2023-01-28 15:19:21 +0100

    Upload files and start jS file

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let liveAuctions = document.querySelector("section.liveAuctions");
+let dataLiveAuctions = [
+    {img: "../img/nftMondrian.png", title: "Mondrian arch", descritpion: "This is simply a memory from the past.", price: 2.87, author: "Daniel White", collection: "Mondrian Flow 2.0", offer: 0.3}
+];
+
+let ticket = document.createElement("div");
