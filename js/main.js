b8891eaffb66008335e5a4bbb42cf4f2542530d4
Nouster <djebali.mohamed88@gmail.com>
2023-01-29 16:13:21 +0100

    Updated data for live auctions sections

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
 let liveAuctions = document.querySelector("section.liveAuctions");
+
 let dataLiveAuctions = [
-    {img: "../img/nftMondrian.png", title: "Mondrian arch", descritpion: "This is simply a memory from the past.", price: 2.87, icon: "../img/iconEth.png", author: "Daniel White", collection: "Mondrian Flow 2.0", offer: 0.3}
+    {img: "../img/nftMondrian.png", title: "Mondrian arch", descritpion: "This is simply a memory from the past. Rainbow mondrian.", price: (Math.random()*10).toFixed(3)  , icon: "../img/iconEth.png", author: "Etik@", collection: "Mondrian Flow 2.0", offer: (Math.random()*10).toFixed(3)},
+    {img: "../img/marylin.gif", title: "Monroe motion", descritpion: "A GIF is worth a book worth of words and art is worth an entire library.", price: (Math.random()*10).toFixed(3), icon: "../img/iconEth.png", author: "Daniel White", collection: "Dall-E", offer: (Math.random()*10).toFixed(3)},
+    {img: "../img/nftGoldQueen.png", title: "Id3entity", descritpion: "Gold Queen : A Journey of Self-Discovery between lands.", price: (Math.random()*10).toFixed(3), icon: "../img/iconEth.png", author: "Daniel White", collection: "Mondrian Flow 2.0", offer: (Math.random()*10).toFixed(3)},
+    {img: "../img/nftOutOfTheBlue.png", title: "Out of the blue", descritpion: "Hands / remember / fingers / Polished surface.", price: (Math.random()*10).toFixed(3), icon: "../img/iconEth.png", author: "Nayima-x01", collection: "Han-Han", offer: (Math.random()*10).toFixed(3)},
+    {img: "../img/fortior.png", title: "Ram-says", descritpion: "Skeleton. Designed using some creative input from Danil Pan.", price: (Math.random()*10).toFixed(3), icon: "../img/iconEth.png", author: "P-BOK", collection: "P-BOK", offer: (Math.random()*10).toFixed(3)},
+    {img: "../img/Efflrescence.gif", title: "EfflØrescence", descritpion: "This is simply a memory from the past.", price: (Math.random()*10).toFixed(3), icon: "../img/iconEth.png", author: "E@gle", collection: "EfflØrescence", offer: (Math.random()*10).toFixed(3)},
+    {img: "../img/nftMondrian.png", title: "Mondrian arch", descritpion: "This is simply a memory from the past. Rainbow mondrian.", price: (Math.random()*10).toFixed(3)  , icon: "../img/iconEth.png", author: "Etik@", collection: "Mondrian Flow 2.0", offer: (Math.random()*10).toFixed(3)},
+    {img: "../img/marylin.gif", title: "Monroe motion", descritpion: "A GIF is worth a book worth of words and art is worth an entire library.", price: (Math.random()*10).toFixed(3), icon: "../img/iconEth.png", author: "Daniel White", collection: "Dall-E", offer: (Math.random()*10).toFixed(3)},
+    {img: "../img/nftGoldQueen.png", title: "Id3entity", descritpion: "Gold Queen : A Journey of Self-Discovery between lands.", price: (Math.random()*10).toFixed(3), icon: "../img/iconEth.png", author: "Daniel White", collection: "Mondrian Flow 2.0", offer: (Math.random()*10).toFixed(3)},
+    {img: "../img/nftOutOfTheBlue.png", title: "Out of the blue", descritpion: "Hands / remember / fingers / Polished surface.", price: (Math.random()*10).toFixed(3), icon: "../img/iconEth.png", author: "Nayima-x01", collection: "Han-Han", offer: (Math.random()*10).toFixed(3)},
+    {img: "../img/fortior.png", title: "Ram-says", descritpion: "Skeleton. Designed using some creative input from Danil Pan.", price: (Math.random()*10).toFixed(3), icon: "../img/iconEth.png", author: "P-BOK", collection: "P-BOK", offer: (Math.random()*10).toFixed(3)},
+    {img: "../img/Efflrescence.gif", title: "EfflØrescence", descritpion: "This is simply a memory from the past.", price: (Math.random()*10).toFixed(3), icon: "../img/iconEth.png", author: "E@gle", collection: "EfflØrescence", offer: (Math.random()*10).toFixed(3)}
 ];
 
 
-dataLiveAuctions.forEach((v)=>{
+let container = document.createElement("div");
+container.className = "liveAuctionsContainer";
+liveAuctions.appendChild(container);
 
-    let container = document.createElement("div");
-    container.className = "liveAuctionsContainer";
-    liveAuctions.appendChild(container);
+dataLiveAuctions.forEach((v)=>{
     let ticket = document.createElement("div");
     ticket.className = "description";
     container.appendChild(ticket);
@@ -18,7 +30,7 @@ dataLiveAuctions.forEach((v)=>{
     ticket.appendChild(picture);
     let ticketTitle = document.createElement("h3");
     ticketTitle.innerHTML = `${v.title}`
-    ticketTitle.className = "my-3";
+    ticketTitle.className = "my-3 text-capitalize";
     ticket.appendChild(ticketTitle);
 
     let ticketDescription = document.createElement("p");
